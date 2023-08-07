e8e41d67df5b62ebedb0c7937768e7454f8987fe
SahajArora <sahajarora1906@gmail.com>
2023-08-08 02:50:43 +0530

    markup + styling done

---
+++ app.js
@@ -0,0 +1,16 @@
+//id="Monthly-traffic"
+//id="Monthly-orders"
+//id="Monthly-revenue"
+
+//id="selected-value"(range)(jo screen me mention hogi)
+
+//id="btn3months"
+//id="btn6months"
+// id="btn9months"
+//id="btn12months"
+
+//class="ROI-percent"(percentage)
+//id="slider-value" (percentage)
+//id="result-amount" (number)
+//class="selected-time (3,6,9,12)
+//id="ROI"(number)
