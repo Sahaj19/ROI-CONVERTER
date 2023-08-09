5e501eca6378983cf4cf28d3a0e6dbc98bc6cb07
SahajArora <sahajarora1906@gmail.com>
2023-08-10 03:22:46 +0530

    ROIPercent : almost done

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
-//Dynamic range setter
-let aboveRange = document.querySelector("#selected-value");
-let belowRange = document.querySelector("#slider-value");
-let slider = document.querySelector("#slider");
+let aboveRange = document.querySelector('#selected-value');
+let belowRange = document.querySelector('#slider-value');
+let slider = document.querySelector('#slider');
 
-aboveRange.textContent = slider.value + "%";
-belowRange.textContent = slider.value + "%";
+aboveRange.textContent = slider.value + '%';
+belowRange.textContent = slider.value + '%';
 
-slider.addEventListener("input", function () {
-  aboveRange.textContent = slider.value + "%";
-  belowRange.textContent = slider.value + "%";
+slider.addEventListener('input', function () {
+  aboveRange.textContent = slider.value + '%';
+  belowRange.textContent = slider.value + '%';
+  finalResult();
 });
-
-//dynamic month setter
+// *********************************************************************
 let finalTime = 6;
-let btns = document.querySelectorAll(".btn");
-let times = document.querySelectorAll(".selected-time");
+let btns = document.querySelectorAll('.btn');
+let times = document.querySelectorAll('.selected-time');
 
 for (let btn of btns) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener('click', function () {
     finalTime = parseInt(btn.textContent.slice(0, 2));
     for (let time of times) {
       time.textContent = btn.textContent;
     }
 
     for (let btn of btns) {
-      btn.classList.remove("focus");
+      btn.classList.remove('focus');
     }
 
-    btn.classList.add("focus");
-    calculateAndDisplayROI();
+    btn.classList.add('focus');
+
+    finalResult();
   });
 }
-
+// **********************************************************************
 for (let time of times) {
   time.textContent = `${finalTime} months`;
 }
 
-let defaultButton = document.querySelector("#btn6months");
+let defaultButton = document.querySelector('#btn6months');
 if (defaultButton) {
-  defaultButton.classList.add("focus");
+  defaultButton.classList.add('focus');
 }
+// *****************************************************************
+function finalResult() {
+  let traffic = parseInt(document.querySelector('#Monthly-traffic').value);
+  let orders = parseInt(document.querySelector('#Monthly-orders').value);
+  let revenue = parseInt(document.querySelector('#Monthly-revenue').value);
+  let sliderValue = parseInt(document.querySelector('#slider').value);
 
-//result calculator
-function calculateAndDisplayROI() {
-  let traffic = parseInt(document.querySelector("#Monthly-traffic").value);
-  let orders = parseInt(document.querySelector("#Monthly-orders").value);
-  let revenue = parseInt(document.querySelector("#Monthly-revenue").value);
-  let sliderValue = parseInt(document.querySelector("#slider").value);
-
-  let result =
-    ((orders / traffic) * (sliderValue / 100) * traffic * (revenue / orders) -
-      revenue) *
-    finalTime;
-
-  console.log("result = ", result);
-
+  // prettier-ignore
+  let result = (((((orders/traffic) * (sliderValue / 100)) * traffic) * (revenue / orders)))* finalTime;
   let cost = finalTime * 3500;
-  console.log("cost", cost);
-
   let ROI = result - cost;
-  console.log("ROI = ", ROI);
+  let ROIPercent = (ROI / result) * 100;
 
-  let ROIPercent = result !== 0 ? (ROI / result) * 100 : 0;
+  let formattedResult = result.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
 
-  // let ROIPercent = (ROI / result) * 100;
-  ROIPercent = Math.min(ROIPercent, 100);
-  console.log("ROIPercent", ROIPercent);
+  let formattedROI = ROI.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
 
-  let RESULT = document.querySelector("#result-amount");
-  // parseInt(RESULT.textContent.toFixed(0));
-  RESULT.textContent = parseInt(result.toFixed(0));
+  // ***********Inserting content into screen********
+  let RESULT = document.querySelector('#result-amount');
+  RESULT.textContent = `${formattedResult}`;
 
-  let roi = document.querySelector("#ROI");
-  // parseInt(roi.textContent.toFixed(0));
-  roi.textContent = parseInt(ROI.toFixed(0));
+  let roi = document.querySelector('#ROI');
+  roi.textContent = `${formattedROI}`;
 
-  let ROIPercentElements = document.querySelectorAll(".ROI-percent");
+  let ROIPercentElements = document.querySelectorAll('.ROI-percent');
   for (let element of ROIPercentElements) {
-    element.textContent = `(${ROIPercent.toFixed(0)}%)`;
+    element.textContent = `(${ROIPercent.toFixed(2)}%)`;
   }
 }
 
-calculateAndDisplayROI();
+finalResult();
