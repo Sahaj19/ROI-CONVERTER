320366e1c9e99b8769e5ba58ea7351cf9fc6ccc4
SahajArora <sahajarora1906@gmail.com>
2023-09-15 16:34:08 +0530

    few minor edits

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ let times = document.querySelectorAll('.selected-time');
 
 for (let btn of btns) {
   btn.addEventListener('click', function () {
+    // console.log('Btn clicked', btn.textContent);
     finalTime = parseInt(btn.textContent.slice(0, 2));
+    // console.log('Updated finalTime:', finalTime);
+
+    // console.log('Btn clicked', finalTime, 'months');
+
     for (let time of times) {
       time.textContent = btn.textContent;
     }
@@ -27,7 +32,6 @@ for (let btn of btns) {
     }
 
     btn.classList.add('focus');
-
     finalResult();
   });
 }
@@ -47,11 +51,28 @@ function finalResult() {
   let revenue = parseInt(document.querySelector('#Monthly-revenue').value);
   let sliderValue = parseInt(document.querySelector('#slider').value);
 
+  //debugging
+  // console.log('traffic', traffic);
+  // console.log('orders', orders);
+  // console.log('revenue', revenue);
+  // console.log('sliderValue', sliderValue);
+  // console.log('finalTime', finalTime);
+
   // prettier-ignore
   let result = (((((orders/traffic) * (sliderValue / 100)) * traffic) * (revenue / orders)))* finalTime;
+  // console.log('result', result);
   let cost = finalTime * 3500;
+  // console.log('cost', cost);
   let ROI = result - cost;
-  let ROIPercent = (ROI / result) * 100;
+  // console.log('ROI', ROI);
+  // let ROIPercent = (ROI / result) * 100;
+  let ROIPercent = result !== 0 ? (ROI / result) * 100 : 0;
+  // console.log('ROIPercent', ROIPercent);
+
+  // console.log('result', result);
+  // console.log('cost', cost);
+  // console.log('ROI', ROI);
+  // console.log('ROIPercent', ROIPercent);
 
   let formattedResult = result.toLocaleString('en-US', {
     style: 'currency',
@@ -68,16 +89,15 @@ function finalResult() {
   });
 
   // ***********Inserting content into screen********
+
   let RESULT = document.querySelector('#result-amount');
   RESULT.textContent = `${formattedResult}`;
 
   let roi = document.querySelector('#ROI');
   roi.textContent = `${formattedROI}`;
 
-  let ROIPercentElements = document.querySelectorAll('.ROI-percent');
-  for (let element of ROIPercentElements) {
-    element.textContent = `(${ROIPercent.toFixed(2)}%)`;
-  }
+  let ROIPercenter = document.querySelector('#ROI-percent');
+  ROIPercenter.innerText = `(${ROIPercent.toFixed(2)}%)`;
 }
 
 finalResult();
